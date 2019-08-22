6670f1fba6dcd233ec695610d579d1c5b6e4f076
Anongnat.rat <anongnat.rat@ku.th>
2019-08-23 02:52:23 +0700

    add test results files

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-
+from time import sleep
 
 
 class MyTestCase(unittest.TestCase):
@@ -21,10 +22,7 @@ class MyTestCase(unittest.TestCase):
     def test_OpenWebSite(self):
         driver = self.driver
         # driver.get("https://fictionlog.co/")
-        if "Fictionlog | นิยายออนไลน์ สุดมันส์ สดใหม่ ฟินทุกรส ครบทุกอารมณ์!" in driver.title:
-            print(driver.title)
-        else:
-            print(driver.title)
+        assert driver.title == "Fictionlog | นิยายออนไลน์ สุดมันส์ สดใหม่ ฟินทุกรส ครบทุกอารมณ์!", "cannot open this site"
 
     def test_Login(self):
         driver = self.driver
@@ -34,7 +32,7 @@ class MyTestCase(unittest.TestCase):
         if username:
             print("login box is founded")
         else:
-            print("login box is not found")
+            print("login box not found")
         username.send_keys(key[0])
         pwd = driver.find_element_by_id("login-password")
         pwd.send_keys(key[1])
@@ -54,7 +52,7 @@ class MyTestCase(unittest.TestCase):
         if 'ค้นหานิยาย, E-book และผู้ใช้ ได้จาก "กล่องค้นหา" ด้านบน' in element.text:
             print("search box is founded")
         else:
-            print("search box is not found")
+            print("search box not found")
 
         def search(msg):
 
@@ -63,8 +61,6 @@ class MyTestCase(unittest.TestCase):
             driver.find_element_by_xpath("//input[@class='SearchForm__Input-sc-1utrbvs-1 dxqRDK']").send_keys(msg,
                                                                                                               Keys.ENTER)
             driver.implicitly_wait(300)
-
-
 
             # print("1")
             has = driver.find_element_by_xpath(
@@ -79,7 +75,7 @@ class MyTestCase(unittest.TestCase):
                     "//div[@class='container Search__FeedBox-b4vuer-0 daudpn']/div[2]/div[1]/a[1]/div[1]/div[2]/div[1]/div[1]").text
 
             print(first)
-            assert msg in first,"maybe this msg be not found or something wrong"
+            assert msg in first, "maybe this msg be not found or something wrong"
 
         search("มังกร")
         search("แมว")
@@ -94,20 +90,21 @@ class MyTestCase(unittest.TestCase):
         if 'ค้นหานิยาย, E-book และผู้ใช้ ได้จาก "กล่องค้นหา" ด้านบน' in element.text:
             print("search box is founded")
         else:
-            print("search box is not found")
+            print("search box not found")
 
         def search(msg):
             driver.find_element_by_xpath("//*[@class='SearchForm__ClearTextButton-sc-1utrbvs-0 jPBmQo']").click()
             driver.find_element_by_xpath("//input[@class='SearchForm__Input-sc-1utrbvs-1 dxqRDK']").send_keys(msg,
                                                                                                               Keys.ENTER)
             driver.implicitly_wait(200)
-            element = driver.find_element_by_xpath("//div[@class='container Search__FeedBox-b4vuer-0 daudpn']/div[2]/div[1]")
+            element = driver.find_element_by_xpath(
+                "//div[@class='container Search__FeedBox-b4vuer-0 daudpn']/div[2]/div[1]")
 
-            assert element.text == "ไม่พบนิยาย","maybe this msg can found in novel's name or something wrong"
+            assert element.text == "ไม่พบนิยาย", "maybe this msg can found in novel's name or something wrong"
 
         search("123456789878")
 
-    def test_loginFail(self):
+    def test_LoginFail(self):
         driver = self.driver
 
         driver.find_element_by_xpath("//div[@class='Navbar__NavbarRightMenuBox-sc-1gjhe9u-8 glkmWR']").click()
@@ -115,14 +112,97 @@ class MyTestCase(unittest.TestCase):
         if username:
             print("login box is founded")
         else:
-            print("login box is not found")
+            print("login box  not found")
         username.send_keys("key[0]")
         pwd = driver.find_element_by_id("login-password")
         pwd.send_keys("key[1]")
         driver.find_element_by_xpath("//div[@class='LoginForm__RowWrapper-sc-1yidlb5-4 bxQUXd']/button[1]").click()
 
         error = driver.find_element_by_xpath("//div[@class='Title__Text-sc-1jzccie-0 Title__H3-sc-1jzccie-3 jNtoco']")
-        assert error.text == "เกิดข้อผิดพลาด","this username and password ขcan login to website"
+        assert error.text == "เกิดข้อผิดพลาด", "this username and password ขcan login to website"
+
+    def test_Logout(self):
+        driver = self.driver
+        key = open("key.txt").read().splitlines()
+        driver.find_element_by_xpath("//div[@class='Navbar__NavbarRightMenuBox-sc-1gjhe9u-8 glkmWR']").click()
+        username = driver.find_element_by_id("login-username")
+        if username:
+            print("login box is founded")
+        else:
+            print("login box not found")
+        username.send_keys(key[0])
+        pwd = driver.find_element_by_id("login-password")
+        pwd.send_keys(key[1])
+        driver.find_element_by_xpath("//div[@class='LoginForm__RowWrapper-sc-1yidlb5-4 bxQUXd']/button[1]").click()
+
+        driver.find_element_by_xpath("//div[@class='Avatar__AvatorBox-sc-1u3bppm-0 gGGrfo']").click()
+        element = driver.find_element_by_xpath("//div[@class='NavbarProfileDropdown__ProfileId-sy9wmn-5 jwRExF']")
+        if "@" + key[0] in element.text:
+            print("Login successful")
+        else:
+            print("Fail")
+
+        logout_btn = driver.find_element_by_xpath(
+            "//div[@class='NavbarProfileDropdown__DropdownModalItemsWrapper-sy9wmn-8 lcEttg']/div[3]")  # หาผิดปุ่ม
+        # print(logout_btn.text)
+        assert logout_btn.text == "ออกจากระบบ", "wrong btn"
+        logout_btn.click()
+        driver.implicitly_wait(30)
+        driver.find_element_by_xpath(
+            "//*[@class='WithoutLoginNavbar__ButtonBox-sc-1wcn44i-0 WithoutLoginNavbar__LoginButton-sc-1wcn44i-1 jpvUIv']").click()
+        txt = driver.find_element_by_xpath("//*[@class='styleModals__ModalTitle-sc-1mu53dz-1 ljhljk']").text
+        assert txt == "เข้าสู่ระบบ", "logout not successful"
+
+    def test_ClickToChangeCategory(self):
+        driver = self.driver
+        topRank = driver.find_element_by_xpath(
+            "//div[@id='__next']/main[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]")
+        assert topRank.text == "อันดับนิยายมาแรง", "wrong btn"
+        topRank.click()
+
+        header = driver.find_element_by_xpath("//div[@class='ExploreForm__CategoryHeader-txiseh-9 buKqdz']/span[1]")
+
+        assert header.text == "เลือกหมวดนิยาย", "something wrong"
+        categories = ['แฟนตาซี', 'กำลังภายใน', 'ไลท์โนเวล', 'กีฬา', 'ไซไฟ', 'ผจญภัย', 'สืบสวน', 'สยองขวัญ', 'สารคดี',
+                      'ชีวิต', 'โรแมนติก', 'ดราม่า', 'รักวัยรุ่น', 'คอเมดี้', 'ย้อนยุค', 'Boy Love / Yaoi',
+                      'Girl Love / Yuri', 'Idol Fic / Fanfic']
+        xpath = "//div[@class='ExploreForm__CategoryList-txiseh-6 kMSoIq']/button[2]"
+        category = driver.find_element_by_xpath(xpath)
+        category.click()
+        print(f"now click {category.text}")
+        # xpath = "//div[@class='ExploreForm__CategoryList-txiseh-6 kMSoIq']/button[3]"
+        # category = driver.find_element_by_xpath(xpath)
+        # category.click()
+        # print(f"now click {category.text}")
+        # enter_btn = driver.find_element_by_xpath("//div[@class='ExploreForm__Footer-txiseh-5 kpvFKN']/div[5]")
+        # actions = ActionChains(driver)
+        # actions.move_to_element(enter_btn)
+        # actions.click(enter_btn)
+        # actions.perform()
+        # print(f"now click {enter_btn.text}")
+        # driver.implicitly_wait(300)
+        # sleep(5)
+        for i in range(3, len(categories) + 2):
+            xpath = "//div[@class='ExploreForm__CategoryList-txiseh-6 kMSoIq']/button[" + str(i) + "]"
+            category = driver.find_element_by_xpath(xpath)
+            category.click()
+            print(f"now click {category.text}")
+            enter_btn = driver.find_element_by_xpath("//div[@class='ExploreForm__Footer-txiseh-5 kpvFKN']/div[5]")
+            enter_btn.click()
+            print(f"now click {enter_btn.text}")
+            sleep(3)
+            last_path = "//div[@class='ExploreForm__CategoryList-txiseh-6 kMSoIq']/button[" + str(i-2) + "]"
+            last_cat = driver.find_element_by_xpath(last_path)
+            first = driver.find_element_by_xpath(
+                "//div[@class='CardItem__FeedItemWrapper-sc-17chdn6-12 honvOT']/div[3]/div[1]/div[2]")
+            while (not(last_cat.text in first.text)) or (not(category.text in first.text)):
+                driver.implicitly_wait(100)
+                first = driver.find_element_by_xpath("//div[@class='CardItem__FeedItemWrapper-sc-17chdn6-12 honvOT']/div[3]/div[1]/div[2]")
+                print(f"now :{first.text}")
+            assert categories[i-2] in first.text
+
+
+
 
     def tearDown(self):
         self.driver.close()
